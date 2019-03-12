663c979a7895091eebb61b86bb2da5b3fd0ed2e9
04513422165 <04513422165@L02409022357.edu.senac.br>
2019-03-12 15:22:32 -0300

    COMMIT DO MC MARCINHO

--- src/View/ListasVendas_Compras_Ordem.java
+++ src/View/ListasVendas_Compras_Ordem.java
@@ -11,9 +11,9 @@ package View;
  */
 public class ListasVendas_Compras_Ordem extends javax.swing.JInternalFrame {
 
-    /**
-     * Creates new form ListasVendas_Compras_Ordem
-     */
+    int aux;
+    
+    
     public ListasVendas_Compras_Ordem() {
         initComponents();
     }
@@ -27,130 +27,180 @@ public class ListasVendas_Compras_Ordem extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-        jButton4 = new javax.swing.JButton();
+        jLista = new javax.swing.JTable();
+        BtnPDF = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
-        jButton5 = new javax.swing.JButton();
+        btnPesquisLista = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
+        cbLv = new javax.swing.JCheckBox();
+        cbOs = new javax.swing.JCheckBox();
+        cbLc = new javax.swing.JCheckBox();
+        txtData1 = new javax.swing.JFormattedTextField();
+        txtData2 = new javax.swing.JFormattedTextField();
 
         setClosable(true);
         setTitle("RELATÓRIOS");
         setFrameIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/Lista.png"))); // NOI18N
 
-        jButton1.setText("Listar Vendas");
-
-        jButton2.setText("Listar Compras");
-
-        jButton3.setText("Listar Ordem de Serviço");
-
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jLista.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {},
+                {},
+                {},
+                {}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(jLista);
 
-        jButton4.setText("PDF");
+        BtnPDF.setText("PDF");
 
         jLabel1.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
         jLabel1.setText("Período :");
 
-        jTextField1.setText("data 1");
-
         jLabel2.setText("até");
 
-        jTextField2.setText("data 2");
-
-        jButton5.setText("pesquisa");
+        btnPesquisLista.setText("pesquisa");
 
         jLabel3.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
         jLabel3.setText("Gerar PDF: ");
+
+        cbLv.setText("Lista Vendas");
+        cbLv.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbLvActionPerformed(evt);
+            }
+        });
+
+        cbOs.setText("Listar Ordem de Serviço");
+        cbOs.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbOsActionPerformed(evt);
+            }
+        });
+
+        cbLc.setText("Listar Compras");
+        cbLc.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbLcActionPerformed(evt);
+            }
+        });
+
+        try {
+            txtData1.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("##/##/####")));
+        } catch (java.text.ParseException ex) {
+            ex.printStackTrace();
+        }
+
+        try {
+            txtData2.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("##/##/####")));
+        } catch (java.text.ParseException ex) {
+            ex.printStackTrace();
+        }
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(27, 27, 27)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1046, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(35, 35, 35)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                 .addComponent(jLabel1)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(txtData1, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(cbLv, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(12, 12, 12)
                                 .addComponent(jLabel2)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jButton5)
-                                .addGap(191, 191, 191)
+                                .addGap(18, 18, 18)
+                                .addComponent(txtData2, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnPesquisLista)
+                                .addGap(18, 18, 18)
                                 .addComponent(jLabel3)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton4))
+                                .addComponent(BtnPDF))
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jButton1)
-                                .addGap(29, 29, 29)
-                                .addComponent(jButton2)
-                                .addGap(30, 30, 30)
-                                .addComponent(jButton3)))))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(cbOs)
+                                .addGap(63, 63, 63)
+                                .addComponent(cbLc)))))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(31, 31, 31)
+                .addGap(22, 22, 22)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2)
-                    .addComponent(jButton3))
-                .addGap(19, 19, 19)
+                    .addComponent(cbLv)
+                    .addComponent(cbOs)
+                    .addComponent(cbLc))
+                .addGap(28, 28, 28)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton5)
-                    .addComponent(jButton4)
-                    .addComponent(jLabel3))
+                    .addComponent(btnPesquisLista)
+                    .addComponent(BtnPDF)
+                    .addComponent(jLabel3)
+                    .addComponent(txtData1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtData2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 474, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(74, Short.MAX_VALUE))
+                .addContainerGap(78, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void cbOsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbOsActionPerformed
+        if (cbOs.isSelected()){
+            cbLv.setSelected(false);
+            cbLc.setSelected(false);
+            aux = 1;
+        }
+        
+    }//GEN-LAST:event_cbOsActionPerformed
+
+    private void cbLvActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbLvActionPerformed
+        if (cbLv.isSelected()){
+            cbOs.setSelected(false);
+            cbLc.setSelected(false);
+            aux = 2;
+        }
+    }//GEN-LAST:event_cbLvActionPerformed
+
+    private void cbLcActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbLcActionPerformed
+        if (cbLc.isSelected()){
+            cbOs.setSelected(false);
+            cbLv.setSelected(false);
+            aux = 3;
+        }
+    }//GEN-LAST:event_cbLcActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
+    private javax.swing.JButton BtnPDF;
+    private javax.swing.JButton btnPesquisLista;
+    private javax.swing.JCheckBox cbLc;
+    private javax.swing.JCheckBox cbLv;
+    private javax.swing.JCheckBox cbOs;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JTable jLista;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
+    private javax.swing.JFormattedTextField txtData1;
+    private javax.swing.JFormattedTextField txtData2;
     // End of variables declaration//GEN-END:variables
 }
